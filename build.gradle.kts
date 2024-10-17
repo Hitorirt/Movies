9f7109af3790aeaf54dc1372628b4a36b3ea327f
trindadedev13 <devsuay@gmail.com>
2024-10-17 20:29:04 -0300

    fix: missing serialization plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.agp.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.compose.compiler) apply false
 }
 
