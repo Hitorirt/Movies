e0fe79de72c384331194e09c2b14a2c2f41c3e92
trindadedev13 <devsuay@gmail.com>
2024-10-17 22:10:02 -0300

    feat: build release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,46 +9,37 @@ plugins {
 android {
     namespace = "dev.trindadedev.movies"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
-    
+
     defaultConfig {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         applicationId = "dev.trindadedev.movies"
         versionCode = 1
         versionName = "1.0"
-        
-        vectorDrawables { 
+
+        vectorDrawables {
             useSupportLibrary = true
         }
     }
-    
+
     compileOptions {
         sourceCompatibility = JavaVersion.toVersion(libs.versions.android.jvm.get().toInt())
         targetCompatibility = JavaVersion.toVersion(libs.versions.android.jvm.get().toInt())
     }
-    
+
     kotlinOptions {
         jvmTarget = libs.versions.android.jvm.get()
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-    
-    buildFeatures {
-        compose = true
-    }
-    
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
     
     signingConfigs {
+        create("release") {
+            // temporary keystore
+            storeFile = file(layout.buildDirectory.dir("../release_key.jks"))
+            storePassword = "release_temp"
+            keyAlias = "release_temp"
+            keyPassword = "release_temp"
+        }
+
         getByName("debug") {
             storeFile = file(layout.buildDirectory.dir("../testkey.keystore"))
             storePassword = "testkey"
@@ -56,15 +47,33 @@ android {
             keyPassword = "testkey"
         }
     }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("release") 
+        }
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
 
 dependencies {
     implementation(platform(libs.compose.bom))
-    
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    
+
     implementation(libs.compose.ui)
     implementation(libs.compose.ui.graphics)
     implementation(libs.compose.activity)
@@ -73,10 +82,10 @@ dependencies {
     implementation(libs.compose.material3)
     implementation(libs.compose.navigation)
     implementation(libs.compose.coil)
-    
+
     implementation(libs.coil.network.okhttp)
-    
+
     implementation(libs.kotlinx.serialization.json)
-    
+
     implementation(libs.google.gson)
 }
